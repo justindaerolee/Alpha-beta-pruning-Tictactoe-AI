c9c0be7e908b868e2ed5fa4221f001cbc529deea
Justin Daero Lee <daero1995lee@gmail.com>
2019-04-27 21:11:07 +1000

    agent.py

--- agent.py
+++ agent.py
@@ -17,6 +17,216 @@ import numpy as np
 boards = np.zeros((10, 10), dtype="int8")
 curr = 0 # this is the current board to play in
 
+'''
+My section
+'''
+INF = 100000
+DEPTH = 1
+
+PLAYER_X = 1
+PLAYER_O = 2
+
+# global function that determines whether start(x) or start(o)
+PLAYER = 0
+OPPONENT = 0
+
+IS_ALPHA_FLAG = 2
+IS_BETA_FLAG = 3
+
+# MinMaxNode class definition
+class MinMaxNode:
+    # flag = 0 (is non-leaf), 1 (leaf), 2 (initial alpha), 3 (intial beta)
+    def __init__(self, prevNode, currState, move, flag):
+        global INF
+        self.prevNode = prevNode
+        self.fullBoard = currState
+        self.move = move
+        self.flag = flag
+        self.score = 0
+
+    # score only needed for leaf nodes and initial alpha and beta nodes
+    def get_score(self):
+        global INF
+        ret = 0
+        if (self.flag == 1) :
+            ret = self.score
+        elif self.flag == 2:
+            ret = - INF
+        elif self.flag == 3 :
+            ret = INF
+        else :
+            sys.exit("Cannot access score of non-leaf nodes")
+        return ret
+
+    def calculate_score(self):
+        self.flag = 1
+        self.score = self.get_heuristic()
+
+    # Wrapper function to calculate the heuristic score of the boards
+    def get_heuristic(self):
+        score_count = 0
+        #row 1
+        score_count += self.check_points([1,2,3])
+        #row 2
+        score_count += self.check_points([4,5,6])
+        #row3
+        score_count += self.check_points([7,8,9])
+        #col1
+        score_count += self.check_points([1,4,7])
+        #col2
+        score_count += self.check_points([2,5,8])
+        #col3
+        score_count += self.check_points([3,6,9])
+        #diagonal top left to bottom right
+        score_count += self.check_points([1,5,9])
+        #diagonal 2
+        score_count += self.check_points([3,5,7])
+        return score_count
+
+    # assigns heuristic based on line
+    def check_points(self, int_arr):
+        global PLAYER
+        global OPPONENT
+        num_player = 0
+        num_oppo = 0
+        ret = 0
+        for j in range(1,10):
+            for i in int_arr:
+                if self.boards[j][i] == PLAYER:
+                    num_player += 1
+                elif self.boards[j][i] == OPPONENT:
+                    num_oppo += 1
+            if (num_player > 0 and num_oppo == 0):
+                if (num_player == 3) :
+                    ret += 1000
+                else :
+                    ret += num_player * 2
+            elif (num_oppo > 0 and num_player == 0):
+                if (num_oppo == 3) :
+                    ret -= 1000
+                else :
+                    ret -= num_oppo * 2
+        return ret
+
+
+    # finds the children of the current board state
+    # one is the Ai's turn
+    def getChildren(self, num):
+        boardsChildren = []
+        for i in range(1,10):
+            if (self.boards[i] == 0):
+                childBoards = np.copy(self.boards)
+                childBoards[i] = num
+                child = MinMaxNode(self, childBoards, i, 0)
+                boardsChildren.append(child)
+        return boardsChildren
+
+    # wrapper function for alpha beta pruning
+    def minmax(self):
+        global INF
+        global DEPTH
+        global IS_ALPHA_FLAG
+        global IS_BETA_FLAG
+        global PLAYER
+        alpha = MinMaxNode(None, None, 0, IS_ALPHA_FLAG)
+        beta = MinMaxNode(None, None, 0, IS_BETA_FLAG)
+        return self.alphabeta(DEPTH, alpha, beta, PLAYER)
+
+    # alpha beta pruning
+    def alphabeta(self, depth, alpha, beta, num):
+        global PLAYER
+        global OPPONENT
+        if depth == 0 or isEnd(self.fullBoard[self.prevNode.move]): # what if the self.prevNode.move == None when depth is set to 1
+            self.calculate_score()
+            return self
+        if num == PLAYER:
+            for child in self.getChildren(PLAYER):
+                childNode = child.alphabeta(depth-1, alpha, beta, OPPONENT)
+                if childNode.get_score() > alpha.get_score() :
+                    alpha = childNode
+                if alpha.get_score() >= beta.get_score() :
+                    break
+            return alpha
+        else:
+            for child in self.getChildren(OPPONENT):
+                childNode = child.alphabeta(depth-1, alpha, beta, PLAYER)
+                if (childNode.get_score() < beta.get_score()) :
+                    beta = childNode
+                if alpha.get_score() >= beta.get_score() :
+                    break
+            return beta
+
+    def find_minmax_move(self):
+        node = self.minmax()
+        tmp = node
+        while node.prevNode is not None:
+            tmp = node
+            node = tmp.prevNode
+        return tmp.move
+
+### End of class Definition
+
+def get_next_best_move(boards):
+    check_players_set() # make sure the players are set
+    m = MinMaxNode(None, boards, 0, 0)
+    return m.find_minmax_move()
+
+
+# checks for one sub board
+def isFull(board):
+    for ele in board:
+        if ele == 0:
+            return False
+    return True
+
+def hasWon(board, num):
+    return (
+        (board[1] == num and board[2] == num and board[3] == num) or
+        (board[4] == num and board[5] == num and board[6] == num) or
+        (board[7] == num and board[8] == num and board[9] == num) or
+        (board[1] == num and board[4] == num and board[7] == num) or
+        (board[2] == num and board[5] == num and board[8] == num) or
+        (board[3] == num and board[6] == num and board[9] == num) or
+        (board[1] == num and board[5] == num and board[9] == num) or
+        (board[3] == num and board[5] == num and board[7] == num)
+        )
+# change later so that we only check hasWon for the player that made the move
+def isEnd(board):
+    if (isFull(board) or hasWon(board, 1) or hasWon(board, 2)):
+        return True
+    return False
+
+# CHECK THAT THE PLAYER AND OPPONENT HAS BEEN SET
+# Doesnt need to be set, just extra precaution
+def check_players_set():
+    global PLAYER
+    global OPPONENT
+    if PLAYER == 1 and OPPONENT == 2:
+        return True
+    elif PLAYER == 2 and OPPONENT == 1:
+        return True
+    sys.exit("player(X) and player(O) not set")
+    return False
+
+# set the global variable of the player and the opponent for the AI.
+def set_player(c):
+    global PLAYER
+    global OPPONENT
+    if (c == "x") :
+        PLAYER = 1
+    elif (c == "o"):
+        PLAYER = 2
+    else:
+        sys.exit("unknown string to set player")
+    if PLAYER == 1 :
+        OPPONENT = 2
+    else :
+        OPPONENT = 1
+    check_players_set()
+'''
+END of mySection
+'''
+
 # print a row
 # This is just ported from game.c
 def print_board_row(board, a, b, c, i, j, k):
@@ -61,6 +271,7 @@ def place(board, num, player):
     global boards
     curr = num
     boards[board][num] = player
+    print_board(board)
 
 # read what the server sent us and
 # only parses the strings that are necessary
@@ -90,6 +301,8 @@ def parse(string):
     elif command == "loss":
         print("We lost :(")
         return -1
+    elif command == "start":
+        set_player(args[0])
     return 0
 
 # connect to socket
